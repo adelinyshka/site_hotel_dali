8ef8d8fe67d3aba19b5164973c501424e309372c
Adel Dubinnikova <adelinyshka@gmail.com>
2019-04-13 19:35:18 +0300

    add build wuth configuration

---
+++ build/webpack.build.conf.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config.js')
+const buildWebpackConfig = merge(baseWebpackConfig, {
+  // BUILD config
+  mode: 'production',
+  plugins: []
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(buildWebpackConfig)
+})
