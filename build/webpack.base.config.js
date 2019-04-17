8a29fe47679a19e2fbc1dfa0d15b7e430591b8a8
Adel Dubinnikova <adelinyshka@gmail.com>
2019-04-17 14:58:23 +0300

    add stars in hotel carousel & return old config.js

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -5,106 +5,105 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
 
 module.exports = {
-  plugins: [new DuplicatePackageCheckerPlugin()]
+    plugins: [new DuplicatePackageCheckerPlugin()]
 };
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ]
 }
 
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets/'
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
 };
 
 module.exports = {
-  // BASE config
-  externals: {
-    paths: PATHS
-  },
-  entry: {
-    app: PATHS.src
-  },
-  output: {
-    filename: `${PATHS.assets}js/[name].js`,
-    path: PATHS.dist,
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ttf|eot|woff|woff2)$/,
-        use: {
-            loader: "file-loader",
-            options: {
-                name: "fonts/[name].[ext]",
-            }
-        }
+    // BASE config
+    externals: {
+        paths: PATHS
     },
-      { 
-        test: /\.pug$/,
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "fonts/[name].[ext]",
+                    }
+                }
+            },
+            {
+                test: /\.pug$/,
                 loader: 'pug-loader',
                 options: {
                     pretty: true
                 }
-      }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test:  /\.(sass|scss)$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
-        }, {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader', options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
-        }
-      ]
-    }]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].css`,
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: `${PATHS.src}/index.pug`,
-      filename: './index.html'
-    }),
-    new CopyWebpackPlugin([
-      { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/static`, to: '' },
-    ])
-  ],
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }, {
+                test:  /\.(sass|scss)$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+                    }, {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader', options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+                    }
+                ]
+            }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.pug`,
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+            { from: `${PATHS.src}/static`, to: '' },
+        ])
+    ],
 };
-
