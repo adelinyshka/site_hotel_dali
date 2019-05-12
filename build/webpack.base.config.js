0d2cbd144e80048f5846c62542d82a6af4f451f3
Adel Dubinnikova <adelinyshka@gmail.com>
2019-05-12 17:16:03 +0300

    delete unused code & block 'discont', change logo, footer & rating

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -58,7 +58,17 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: '/node_modules/'
-            }, {
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                },{
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            },{
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'file-loader',
                 options: {
